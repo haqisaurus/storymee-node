3b645a683a241db64db01f0e4b1774a0b47a7d33
LAPTOP-BF18BV75\talenthrm <haqisaurus@yahoo.com>
2021-12-28 15:32:30 +0700

    - update env

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
-
+const mongoUrl: any = process.env.MONGO_DB;
 export const connectDB = async () => {
-    await mongoose.connect("mongodb://localhost:27017/storymee");
-    // await mongoose.connect(
-    //     "mongodb://storymee:tekanenter@cluster0-shard-00-00.lz19i.mongodb.net:27017,cluster0-shard-00-01.lz19i.mongodb.net:27017,cluster0-shard-00-02.lz19i.mongodb.net:27017/storymee?ssl=true&replicaSet=atlas-ifjf71-shard-0&authSource=admin&retryWrites=true&w=majority"
-    // );
+    await mongoose.connect(mongoUrl);
 };
