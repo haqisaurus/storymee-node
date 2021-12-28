d9e484c1c0921be497158013806c304cb2673cc1
LAPTOP-BF18BV75\talenthrm <haqisaurus@yahoo.com>
2021-12-28 15:44:10 +0700

    - update running

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 const mongoUrl: any = process.env.MONGO_DB;
+console.log(mongoUrl);
 export const connectDB = async () => {
     await mongoose.connect(mongoUrl);
 };
